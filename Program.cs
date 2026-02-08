f615c5506a963da33b51c35a4a734d37588e91a3
RashfiTabassum <rashfi2004@gmail.com>
2026-02-09 04:35:41 +0600

    fixed

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-
+using System.IO;
+//using UserManagementApp.Data; // replace with your namespace
+//using UserManagementApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,19 +13,26 @@ builder.Services.AddControllersWithViews();
 //builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //    options.UseSqlServer(
 //        builder.Configuration.GetConnectionString("DefaultConnection")));
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Database: SQLite in production, SQL Server locally
+// ============================
+var sqlitePath = "/app/Data/UserManagementApp.db";
+
+// Ensure directory exists in container
+//var dataDir = "/app/Data";
+//if (!Directory.Exists(dataDir))
+//{
+//    Directory.CreateDirectory(dataDir);
+//}
 
 if (builder.Environment.IsDevelopment())
 {
-    // Local: SQL Server
     builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(connectionString));
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 }
 else
 {
-    // Production: SQLite
     builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlite(connectionString));
+        options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection")));
 }
 
 
@@ -43,6 +52,24 @@ builder.Services.AddSingleton<EmailService>();
 
 
 var app = builder.Build();
+
+// Automatically create/migrate the database on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    if (builder.Environment.IsDevelopment())
+    {
+        // Use migrations for SQL Server locally
+        db.Database.Migrate();
+    }
+    else
+    {
+        // SQLite in Render container: create DB if not exists
+        db.Database.EnsureCreated();
+    }
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
